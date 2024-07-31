c992b09430dbe5dbe6fa44caffdaa47dffdfd4b4
鋒 <100204150+illusory1286@users.noreply.github.com>
2024-07-31 17:17:18 +0800

    使用auth.js 寫死帳密 LoginForm.js 引用validateCredentials函數做 if判斷來驗證帳密是否符合

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Form, Input, Button, Checkbox } from 'antd';
 
+import { validateCredentials } from '../../utils/auth';
+
 const onFinish = (values) => {
   console.log('Success:', values);
   console.log('Email:', values.username);
   console.log('Password:', values.password);
-  if (values.remember) {
-    document.cookie = `email=${values.username}; max-age=31536000; path=/`; // 1年
+  if (validateCredentials(values.username, values.password)) {
+    console.log('Login successful!');
+    // if (values.remember) {
+    //   document.cookie = `email=${values.username}; max-age=31536000; path=/`; // 1年
+    // }
+    // 執行跳轉邏輯
+  } else {
+    console.log('Invalid credentials');
   }
 };
 
@@ -15,75 +23,75 @@ const onFinishFailed = (errorInfo) => {
 };
 
 const LoginForm = ({ formType }) => {
-  return (
-    <Form
-      name="login"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-      style={{ maxWidth: 400 }}
-    >
-    {formType === 'login' ? (
-    <>    
-        <Form.Item
-        label="Identity(E-mail)"
-        name="username"
-        rules={[
-          { type: 'email', message: 'The input is not valid E-mail!' },
-          { required: true, message: 'Please input your username!' },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{ offset: 8, span: 16 }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-    </>
-    ):(
-        <>
+    return (
+        <Form
+        name="login"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        // initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        style={{ maxWidth: 400 }}
+        >
+        {formType === 'login' ? (
+        <>    
             <Form.Item
-                label="Name"
-                name="name"
-                rules={[{ required: true, message: 'Please input your name!' }]}
-            >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="Identity(E-mail)"
-                name="username"
-                rules={[
-                { type: 'email', message: 'The input is not valid E-mail!' },
-                { required: true, message: 'Please input your username!' },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-        </>
-        )}        
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-            {formType === 'login' ? 'Login' : 'Send'}
-            </Button>
+            label="Identity(E-mail)"
+            name="username"
+            rules={[
+            { type: 'email', message: 'The input is not valid E-mail!' },
+            { required: true, message: 'Please input your username!' },
+            ]}
+        >
+            <Input />
         </Form.Item>
-    </Form>
-  );
+
+        <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+            <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+        >
+            <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+        </>
+        ):(
+            <>
+                <Form.Item
+                    label="Name"
+                    name="name"
+                    rules={[{ required: true, message: 'Please input your name!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Identity(E-mail)"
+                    name="username"
+                    rules={[
+                    { type: 'email', message: 'The input is not valid E-mail!' },
+                    { required: true, message: 'Please input your username!' },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+            </>
+            )}        
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                {formType === 'login' ? 'Login' : 'Send'}
+                </Button>
+            </Form.Item>
+        </Form>
+    );
 };
 
 export default LoginForm;
---
+++ src/utils/auth.js
@@ -0,0 +1,8 @@
+// utils/auth.js
+export const validateCredentials = (username, password) => {
+    const hardcodedUsername = "admin@example.com";
+    const hardcodedPassword = "password123";
+  
+    return username === hardcodedUsername && password === hardcodedPassword;
+  };
+  
