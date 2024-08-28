793a7b662de9c6d8f5fa56b19edec0607e196b33
鋒 <100204150+illusory1286@users.noreply.github.com>
2024-08-28 11:57:39 +0800

    Add: Create Setting function UI 1. 新增Attack UI畫面 2. 新增IntermediaryServerForm UI畫面 3. 新增System UI畫面

--- src/components/Platform/Settings/System.js
+++ src/components/Platform/Settings/System.js
@@ -1,12 +1,46 @@
 import React from 'react'
+import { Layout , theme ,Card } from 'antd';
+const { Content } = Layout;
 
 export const System = () => {
+  const {
+    token: { borderRadiusLG },
+  } = theme.useToken();
+
   return (
     <>
-      <div>Name：DDos</div>
-      <div>System Version：1.x.x</div> {/* /之後改為變數去抓/ */}
-      <div>Release Version：1.x.x</div>
-      <div>Release Date：xxxx-xx-xx</div>
+      <Layout>
+        <Content
+          style={{
+            padding: '0 48px',
+          }}
+        >
+          {/* <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>Setting</Breadcrumb.Item>
+            <Breadcrumb.Item>System</Breadcrumb.Item>
+          </Breadcrumb> */}
+            
+          <h1>System</h1>
+          <div
+            style={{
+              minHeight: 380,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Card bordered={false} style={{ width: 850 } }>
+              <p>Name：DDos</p>
+              <p>System Version：1.x.x</p> {/* /之後改為變數去抓/ */}
+              <p>Release Version：1.x.x</p>
+              <p>Release Date：xxxx-xx-xx</p>
+            </Card>
+          </div>
+        </Content>
+      </Layout>
     </>
   )
 }
